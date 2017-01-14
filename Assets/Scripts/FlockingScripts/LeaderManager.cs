3242f5ec0dd73d73cec55cba3bdcd175203e96f4
mikebjoyce <mikebjoyce@gmail.com>
2017-01-14 00:14:30 -0500

    flocker and caribou scene
    a bit buggy

---
+++ Assets/Scripts/FlockingScripts/LeaderManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LeaderManager : MonoBehaviour
+{
+	public List<GameObject> leaders = new List<GameObject>();
+	// Use this for initialization
+	void Start()
+	{
+		List<GameObject> listGos = GetComponentsInChildren<GameObject> ();
+		foreach (GameObject t in listGos) 
+		{
+			leaders.Add (t);
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
