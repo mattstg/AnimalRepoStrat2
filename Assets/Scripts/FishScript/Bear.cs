5cb5e4b1ea3ce31d648f48ac2796f2bb26268f37
mattstg <mattstger@gmail.com>
2017-01-19 19:10:52 -0500

    Music, Bears

---
+++ Assets/Scripts/FishScript/Bear.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bear : MonoBehaviour {
+
+	public List<Transform> bearSwipes;
+	public Transform swipeParent;
+	// Use this for initialization
+	void Awake() {
+		bearSwipes = new List<Transform> ();
+		foreach (Transform t in swipeParent) {
+			bearSwipes.Add (t);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
