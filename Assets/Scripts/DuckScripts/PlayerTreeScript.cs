cee6454a63ccab48304ac369c7cd76d5ba907f50
mattstg <mattstger@gmail.com>
2017-01-26 17:37:45 -0500

    Trees, Rivers

---
+++ Assets/Scripts/DuckScripts/PlayerTreeScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTreeScript : MonoBehaviour {
+
+    //List<Transform> activeCollisions = new List<Transform>();
+
+    /*public void OnCollisionEnter2D(Collision2D coli)
+    {
+        ResolveCollision(coli.gameObject, true);
+    }
+    
+    public void OnCollisionExit2D(Collision2D coli)
+    {
+        ResolveCollision(coli.gameObject, false);
+    }
+   
+    */
+
+    public void OnTriggerExit2D(Collider2D coli)
+    {
+        ResolveCollision(coli.gameObject, false);
+    }
+
+    public void OnTriggerEnter2D(Collider2D coli)
+    {
+        ResolveCollision(coli.gameObject, true);
+    }
+
+    private void ResolveCollision(GameObject go, bool entering)
+    {
+        if (go.CompareTag("Tree"))
+        {
+            if (!entering)
+            {
+                SetAllLeafTransparency(go, 1f);
+            }
+            else
+            {
+                SetAllLeafTransparency(go, .2f);
+            }
+        }
+    }
+
+    private void SetAllLeafTransparency(GameObject tree,float toSet)
+    {
+        foreach (Transform t in tree.transform)
+        {
+            if (t.name == "Leaves")
+            {
+                Color c = t.GetComponent<SpriteRenderer>().color;
+                c.a = toSet;
+                t.GetComponent<SpriteRenderer>().color = c;
+            }
+        }
+    }
+}
--- Assets/Scripts/DuckScripts/WaterStream.cs
+++ Assets/Scripts/DuckScripts/WaterStream.cs
@@ -7,19 +7,21 @@ public class WaterStream : MonoBehaviour {
     public float forceOfStream = 0;
     public Vector2 dirOfStream = new Vector2(1,0);
 
-	public void OnCollisionEnter2D(Collision2D coli)
+	public void OnCollisionStay2D(Collision2D coli)
     {
         if(coli.gameObject.GetComponent<Rigidbody2D>())
         {
-            coli.gameObject.GetComponent<Rigidbody2D>().AddForce(dirOfStream * forceOfStream*Time.deltaTime,ForceMode2D.Impulse);
+			Vector2 p = coli.gameObject.transform.position;
+			coli.gameObject.transform.position = Vector2.MoveTowards (p, p + dirOfStream * forceOfStream, forceOfStream * Time.deltaTime);
         }
     }
 
-    public void OnTriggerEnter2D(Collider2D coli)
+    public void OnTriggerStay2D(Collider2D coli)
     {
-        if (coli.gameObject.GetComponent<Rigidbody2D>())
-        {
-            coli.gameObject.GetComponent<Rigidbody2D>().AddForce(dirOfStream * forceOfStream * Time.deltaTime, ForceMode2D.Impulse);
-        }
+		if(coli.gameObject.GetComponent<Rigidbody2D>())
+		{
+			Vector2 p = coli.gameObject.transform.position;
+			coli.gameObject.transform.position = Vector2.MoveTowards (p, p + dirOfStream * forceOfStream, forceOfStream * Time.deltaTime);
+		}
     }
 }
