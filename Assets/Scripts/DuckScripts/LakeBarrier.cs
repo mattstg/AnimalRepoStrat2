b3a9a621b097378f946be7d6aa6da2948e6cd407
mattstg <mattstger@gmail.com>
2017-02-01 01:11:25 -0500

    Graphic redo of duck level

---
+++ Assets/Scripts/DuckScripts/LakeBarrier.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LakeBarrier : MonoBehaviour {
+
+    float forceOfStream = 3;
+    Vector2 dirOfStream = new Vector2(0, -1);
+
+    public void OnCollisionStay2D(Collision2D coli)
+    {
+        if (coli.gameObject.GetComponent<Duckling>())
+        {
+            Vector2 p = coli.gameObject.transform.position;
+            coli.gameObject.transform.position = Vector2.MoveTowards(p, p + dirOfStream * forceOfStream, forceOfStream * Time.deltaTime);
+        }
+    }
+
+    public void OnTriggerStay2D(Collider2D coli)
+    {
+        if (coli.gameObject.GetComponent<Duckling>())
+        {
+            Vector2 p = coli.gameObject.transform.position;
+            coli.gameObject.transform.position = Vector2.MoveTowards(p, p + dirOfStream * forceOfStream, forceOfStream * Time.deltaTime);
+        }
+    }
+}
