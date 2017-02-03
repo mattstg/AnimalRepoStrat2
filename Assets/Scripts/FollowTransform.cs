f55f5eedb0d90742626242de7f7e6cd7272cc1a6
mattstg <mattstger@gmail.com>
2017-02-03 18:37:59 -0500

    fsdf
    sdfdsf

---
+++ Assets/Scripts/FollowTransform.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTransform : MonoBehaviour {
+
+    public Transform toFollow;
+
+	public void Update()
+    {
+        if (toFollow)
+            transform.position = new Vector3(toFollow.position.x, toFollow.position.x, 0);
+    }
+}
