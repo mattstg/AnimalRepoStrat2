9003de357d3f7578f0908fe68435d790fc480b53
isomorphica <chevalier.jared@gmail.com>
2017-02-07 11:29:23 -0500

    smooth camera follow (fish scene)
    modified the fish scene camera script to make the camera motion smoother
    and less rigidly centered on the player fish. similar updates to follow
    for other scenes.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour {
 
-	public Transform toFollow;
+    public float dampTime = 0.5f;
+    private Vector3 velocity = Vector3.zero;
+    public Transform toFollow;
     public Vector3 offset;
 
 	// Update is called once per frame
 	void Update () {
 		if (toFollow) {
-			transform.position = new Vector3(toFollow.position.x + offset.x, toFollow.position.y + offset.y, -10);
-		}
+            //transform.position = new Vector3(toFollow.position.x + offset.x, toFollow.position.y + offset.y, -10);
+
+            Vector3 target = new Vector3(toFollow.position.x + offset.x, toFollow.position.y + offset.y, -10);
+            Vector3 point = GetComponent<Camera>().WorldToViewportPoint(target);
+            Vector3 delta = target - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
+            Vector3 destination = transform.position + delta;
+            transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+        }
 	}
 
 	public void SetZoom(float z)
