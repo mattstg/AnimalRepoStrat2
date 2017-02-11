8195e6a9674b1b547d27ecac3069cdf1082b1771
isomorphica <chevalier.jared@gmail.com>
2017-02-11 15:25:10 -0500

    opacity fade + calculus = smooth changes

--- Assets/Scripts/OpacityFade.cs
+++ Assets/Scripts/OpacityFade.cs
@@ -6,6 +6,7 @@ public class OpacityFade : MonoBehaviour {
 
     private float fadeDuration = 1;
     private float fadeProgress = 1;
+    private float sourceOpacity = 1;
     private float targetOpacity = 1;
 
 	void Start ()
@@ -22,20 +23,56 @@ public class OpacityFade : MonoBehaviour {
         {
             if (fadeDuration <= 0)
                 fadeDuration = 1;
-            float delta = Time.deltaTime / (fadeDuration - fadeProgress);
-            float newOpacity = presentOpacity + (targetOpacity - presentOpacity) * delta;
+
+            //float delta = Time.deltaTime / (fadeDuration - fadeProgress);
+            //float newOpacity = presentOpacity + (targetOpacity - presentOpacity) * delta;
+
+            float integral = GetIntegral(fadeProgress / fadeDuration);
+            float newOpacity = targetOpacity * integral + sourceOpacity * (1 - integral);
             fadeProgress += Time.deltaTime;
             if (fadeProgress > fadeDuration)
             {
-                newOpacity = targetOpacity;
                 fadeProgress = fadeDuration;
             }
             ApplyOpacity(newOpacity);
         }
 	}
 
+    public float GetVelocity(float progress)
+    {
+        if (progress >= 0 && progress <= 1)
+        {
+            return -4 * Mathf.Abs(progress - 0.5f) + 2;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    public float GetIntegral(float progress)
+    {
+        if (progress < 0)
+        {
+            return 0;
+        }
+        else if (progress > 1)
+        {
+            return 1;
+        }
+        else if (progress <= 0.5)
+        {
+            return progress * GetVelocity(progress) / 2;
+        }
+        else
+        {
+            return 0.5f + ((progress - 0.5f) * (GetVelocity(progress) + 2) / 2);
+        }
+    }
+
     public void SetTargetOpacity(float target, float duration)
     {
+        sourceOpacity = GetPresentOpacity();
         targetOpacity = target;
         fadeProgress = 0;
         fadeDuration = duration;
