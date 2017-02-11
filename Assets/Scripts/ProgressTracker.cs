5ffcaa1ceee757d4df362f0ce31b45012ce696a8
mattstg <mattstger@gmail.com>
2017-02-11 17:24:58 -0500

    Progress tracker, Fish as triggers
    fish trigger colliders
    Progress tracker framework

---
+++ Assets/Scripts/ProgressTracker.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressTracker {
+
+	private static ProgressTracker instance;
+
+
+
+	public static ProgressTracker Instance
+	{
+		get 
+		{
+			if (instance == null)
+			{
+				instance = new ProgressTracker();
+			}
+			return instance;
+		}
+	}
+
+	float[] roundScores = new float[5];
+	float[] roundMult = new float[5];
+
+	private ProgressTracker()
+	{
+		for (int i = 0; i < 5; i++) 
+		{
+			roundScores [i] = 0;
+			roundMult [i] = 0;
+		}
+	}
+
+	public void SetRoundScore(float score, int round)
+	{
+		roundScores [round] = score;
+	}
+
+	public void SetRoundMult(float score, int round)
+	{
+		roundMult [round] = score;
+	}
+
+	public float SubmitProgress()
+	{
+		float totalScore = 0;
+		for (int i = 0; i < 5; i++) 
+		{
+			totalScore += roundScores [i] *	roundMult [i];
+		}
+		return totalScore;
+	}
+}
