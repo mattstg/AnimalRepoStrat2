baa18b1fa99c12ad608d2d0c3adf8188b46797b0
mattstg <mattstger@gmail.com>
2017-02-11 22:51:00 -0500

    Caribou
    Caribou lakes slow down caribou and wolves
    Caribou, wolves and calves moved to correct sorting layer
    Lakes added colliders
    Mask for Trodden path added

---
+++ Assets/Scripts/Caribou/LakeEffector.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LakeEffector : MonoBehaviour {
+
+	public void OnCollisionEnter2D(Collision2D coli)
+    {
+        ResolveCollision(coli.gameObject, true);
+    }
+
+    public void OnTriggerEnter2D(Collider2D coli)
+    {
+        ResolveCollision(coli.gameObject, true);
+    }
+
+    public void OnCollisionExit2D(Collision2D coli)
+    {
+        ResolveCollision(coli.gameObject, false);
+    }
+
+    public void OnTriggerExit2D(Collider2D coli)
+    {
+        ResolveCollision(coli.gameObject, false);
+    }
+
+    private void ResolveCollision(GameObject go, bool entering)
+    {
+        if (go.GetComponent<PlayerCaribou>())
+        {
+            go.GetComponent<PlayerCaribou>().speed = (entering) ? 5 : 12;
+        }
+        else if (go.GetComponent <FlockingAI>())
+        {
+            go.GetComponent<FlockingAI>().waterSlowCoeff = (entering) ? .3f : 1f;
+        }
+    }
+}
