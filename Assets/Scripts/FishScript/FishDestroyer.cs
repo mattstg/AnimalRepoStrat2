9b6e9b93387020d328204b87887df6ead0852d1a
mikebjoyce <mikebjoyce@gmail.com>
2017-02-12 18:44:03 -0500

    Fish Destroyed when get too far
    > Fish Destroyer Script checks with player position to see if too far
    away
    > Every second has percent chance of testing (currently 2.5%);

---
+++ Assets/Scripts/FishScript/FishDestroyer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishDestroyer : MonoBehaviour {
+	public Transform targetTransfrom;
+	public float disntanceToDestroy = 25;
+	public float prcntChance = 2.5f; // per seond
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Random.Range (0, 100) < (prcntChance * Time.deltaTime) && targetTransfrom != null) {
+			tryToDestroy ();
+		}
+	}
+
+	public void tryToDestroy(){
+		foreach (Fish t in GetComponentsInChildren<Fish>()) {
+			if(Vector3.Distance(targetTransfrom.position,t.transform.position) > disntanceToDestroy){
+				Destroy (t.gameObject);
+			}
+		}
+	}
+}
