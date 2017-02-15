71727ba9d9b124ffb51d85853c1a62678733c002
braChevalier <braden.chevalier@gmail.com>
2017-02-14 23:58:22 -0500

    Duck river; river direction script

---
+++ Assets/Scripts/DirectionalStream.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionalStream : WaterStream {
+    public void SetDirection()
+    {
+        Quaternion quat = GetComponent<Transform>().rotation;
+        Vector2 rot = quat * Vector2.left;
+        
+        GetComponent<WaterStream>().dirOfStream = rot;
+    }
+
+	// Use this for initialization
+	void Start () {
+        SetDirection();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
