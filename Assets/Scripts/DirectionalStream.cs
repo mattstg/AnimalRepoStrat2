0f4847b441153f53f8c60387de8294159489c408
braChevalier <braden.chevalier@gmail.com>
2017-02-15 12:41:36 -0500

    fix for flipped streams (dont ask)

--- Assets/Scripts/DirectionalStream.cs
+++ Assets/Scripts/DirectionalStream.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DirectionalStream : WaterStream {
+    public bool isFlippedAroundYAxis = false;
+
     public void SetDirection()
     {
         Quaternion quat = GetComponent<Transform>().rotation;
-        Vector2 rot = quat * Vector2.right;
-        
+        Vector2 rot = Vector2.zero;
+        if (isFlippedAroundYAxis)
+        {rot = quat * Vector2.right; }
+        else if (!isFlippedAroundYAxis)
+        {rot = quat * Vector2.left; }
+
         GetComponent<WaterStream>().dirOfStream = rot;
     }
 
