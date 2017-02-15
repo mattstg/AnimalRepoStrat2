a3de11102cea157eb6bac4af37508ef61b683666
mikebjoyce <mikebjoyce@gmail.com>
2017-02-14 20:52:09 -0500

    Bower auto drop in own bower / can't pick own items up

--- Assets/Scripts/BowerBirdScript/PlayerBowerBird.cs
+++ Assets/Scripts/BowerBirdScript/PlayerBowerBird.cs
@@ -21,10 +21,20 @@ public class PlayerBowerBird : BowerBird {
 		if (altitude != 0) {
 			autoPickup = true;
 		}
+
+		if (isHolding && isInBower() && altitude == 0) {
+			DropItem ();
+		}
+
 	}
 
 	//just overwriding parent's functions used for bird AI, to do nothing
 	public void returnHome(){}
 	public void IdleBoredom(float timeIdle){}
 	public void SeekRandomItem(){}
+
+	public bool isInBower(){
+		Vector3 posB = bower.transform.position;
+		return Vector3.Distance (posB, transform.position) < 4;
+	}
 }
