fc80aee1a22b6159e236d71f69269e7ac932ccbb
isomorphica <chevalier.jared@gmail.com>
2017-02-23 02:25:19 -0500

    fish, trees, ferns, things, stuff

--- Assets/Scripts/BowerBirdScript/randomness.cs
+++ Assets/Scripts/BowerBirdScript/randomness.cs
@@ -5,12 +5,15 @@ using UnityEngine; using LoLSDK;
 public class randomness : MonoBehaviour {
 
 	public Vector2 randomScale = new Vector2 (-.4f, .4f);
-	// Use this for initialization
+    private float scaleFactor;
+    // Use this for initialization
 	void Start () {
 		Transform mink = GetComponent<Transform> ();
 		mink.Rotate(new Vector3(0,0, Random.Range (0, 360)));
-		mink.localScale = mink.localScale + mink.localScale * Random.Range(randomScale.x,randomScale.y);
-		Destroy (this); //no longer need script
+        scaleFactor = 1 + Random.Range(randomScale.x, randomScale.y);
+        mink.localScale = new Vector3(mink.localScale.x * scaleFactor, mink.localScale.y * scaleFactor, 1);
+        //mink.localScale = mink.localScale + mink.localScale * Random.Range(randomScale.x,randomScale.y);
+        Destroy(this); //no longer need script
 	}
 
 }
