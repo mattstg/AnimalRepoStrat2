27c47e1df5c5cd24dd6c20408017de31e388dcc7
isomorphica <chevalier.jared@gmail.com>
2017-02-22 20:10:24 -0500

    bower direction arrow

--- Assets/Scripts/BowerBirdScript/bowerDirection.cs
+++ Assets/Scripts/BowerBirdScript/bowerDirection.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class bowerDirection : MonoBehaviour {
 	public PlayerBowerBird player;
+    public Camera cam;
 
 	// Use this for initialization
 	void Start () {
@@ -15,8 +16,15 @@ public class bowerDirection : MonoBehaviour {
 		rotate ();
 	}
 
-	private void rotate(){
-		Vector3 dirVector = player.bower.transform.position - player.transform.position;
+	private void rotate()
+    {
+        Vector3 bowerPos = cam.GetComponent<Camera>().WorldToViewportPoint(player.bower.transform.position);
+        Vector3 arrowPos = cam.GetComponent<Camera>().ScreenToViewportPoint(transform.position);
+        Vector3 dirVector = bowerPos - arrowPos;
+        Debug.Log(player.bower.transform.position + ", " + transform.position + ", " + bowerPos + ", " + arrowPos + ", " + dirVector);
+
+//      Vector3 dirVector = player.bower.transform.position - player.transform.position;
+
 		float angle = Mathf.Atan2 (dirVector.y, dirVector.x) * Mathf.Rad2Deg;
 		Quaternion q = Quaternion.AngleAxis (angle + 270,Vector3.forward);
 		transform.rotation = q;
